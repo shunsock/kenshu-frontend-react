c0cc6ac5b638e6e83b6e2c60f2420c3066c2ddb2
ShunDeveloper <shunsuke.tsuchiya@prtimes.co.jp>
2023-05-29 18:17:49 +0900

    callbackから関数名のみの記述に変更しました

--- app/src/form.tsx
+++ app/src/form.tsx
@@ -25,12 +25,12 @@ export const Form = () => {
         placeholder="enter your task"
         value={inputText}
         className="border-2 px-[3%] py-[1%] text-lg w-[500px] my-[1%]  border-comment rounded-md"
-        onChange={(e) => {handleInput(e)}}
+        onChange={handleInput}
       />
       <button
         type="submit"
         className="ml-[3%] bg-orange text-lg text-white px-[3%] py-[1%] rounded-md"
-        onClick={() => {handleSubmit}}
+        onClick={handleSubmit}
       >
         create!
       </button>
