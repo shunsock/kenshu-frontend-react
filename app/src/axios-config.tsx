abba435fad31cb140eed21a87cc3a4af9f548a03
ShunDeveloper <shunsuke.tsuchiya@prtimes.co.jp>
2023-05-29 16:00:11 +0900

    headerの不要な部分を削除しました

--- app/src/axios-config.tsx
+++ app/src/axios-config.tsx
@@ -2,8 +2,4 @@ import axios from 'axios';
 
 export const client = axios.create({
   baseURL: 'http://localhost:8000',
-  headers: {
-    'Content-Type': 'application/json;charset=utf-8',
-    'Access-Control-Allow-Origin': '*',
-  },
 });
