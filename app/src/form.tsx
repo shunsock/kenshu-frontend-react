f60c9c7839642aed70324ca6ff1ffa0f80a84741
ShunDeveloper <shunsuke.tsuchiya@prtimes.co.jp>
2023-05-29 17:13:16 +0900

    onClick, onInputの書き方を修正しました

--- app/src/form.tsx
+++ app/src/form.tsx
@@ -7,14 +7,12 @@ export const Form = () => {
   const [inputText, setInputText] = useState<string>("");
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setInputText(e.target.value);
-    console.log("text was changed", inputText);
   }
 
   const mutation = useMutation({
     mutationFn: createTask
   });
   const handleSubmit = (): void => {
-    console.log("form was submitted");
     mutation.mutate();
   }
 
@@ -25,12 +23,12 @@ export const Form = () => {
         placeholder="enter your task"
         value={inputText}
         className="border-2 px-[3%] py-[1%] text-lg w-[500px] my-[1%]  border-comment rounded-md"
-        onChange={(e) => { handleInput(e) }}
+        onChange={() => {handleInput}}
       />
       <button
         type="submit"
         className="ml-[3%] bg-orange text-lg text-white px-[3%] py-[1%] rounded-md"
-        onClick={() => { handleSubmit() }}
+        onClick={() => {handleSubmit}}
       >
         create!
       </button>
