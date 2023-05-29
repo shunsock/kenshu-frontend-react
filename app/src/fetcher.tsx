3a76ba9bbe7801b3ea6d4241fdf7db4bc7ba8109
ShunDeveloper <shunsuke.tsuchiya@prtimes.co.jp>
2023-05-29 18:13:51 +0900

    http//127...を省略しました

--- app/src/fetcher.tsx
+++ app/src/fetcher.tsx
@@ -3,14 +3,14 @@ import { Task } from './types/my-type';
 
 export const getAllData = async () => {
   const { data } = await client.get<{ tasks: Task[] }>(
-      'http://127.0.0.1:8000/api/tasks',
+      '/api/tasks',
   );
   return data.tasks
 };
 
 export const createTask = async () => {
     await client.post(
-      'http://127.0.0.1:8000/api/tasks',
+      '/api/tasks',
       {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
