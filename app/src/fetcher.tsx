e5f3402958f79e6885ca18b60e3b57bb77231a1c
ShunDeveloper <shunsuke.tsuchiya@prtimes.co.jp>
2023-05-29 15:00:15 +0900

    taskの型をaxiosでGETするタイミングで付与するようにしました

--- app/src/fetcher.tsx
+++ app/src/fetcher.tsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { config } from './axios-config';
+import { Task } from './types/my-type';
 
 export const getAllData = async () => {
-  const { data } = await axios.get(
+  const { data } = await axios.get<{ tasks: Task }>(
     'http://127.0.0.1:8000/api/tasks',
     config
   )
